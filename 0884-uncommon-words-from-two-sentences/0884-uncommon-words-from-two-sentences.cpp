82c273161a520a8b314044a2107eff98e3fdece2
shishupalsahu <149509008+shishupalsahu@users.noreply.github.com>
2024-09-21 01:29:09 +0530

    Time: 0 ms (100%), Space: 8.8 MB (69.66%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
@@ -15,7 +14,6 @@ public:
         };
         addWords(s1);
         addWords(s2);
-
         for (const auto& [word, count] : wordCount) {
             if (count == 1) {
                 result.push_back(word);
